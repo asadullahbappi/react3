f3838d06c671599cbfb0bb3ab9e7e9bd87b93e37
asadullahbappi <58213620+asadullahbappi@users.noreply.github.com>
2020-06-08 09:13:21 -0400

    build React app

---
+++ src/dinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dinner () {
+    return(
+
+        <div>
+        <h1> Today we are serving chicken biryani</h1>
+        <h1> Today we are serving chicken kharai</h1>
+        </div>
+    )
+}
+
+export default Dinner;
